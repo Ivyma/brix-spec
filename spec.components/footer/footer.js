3698dea2fce5be8036734ef7a8a87ca6c6aea6e4
墨智 <mozhi.gyy@alibaba-inc.com>
2015-07-27 16:52:35 +0800

    + footer

--- spec.components/footer/footer.js
+++ spec.components/footer/footer.js
@@ -11,14 +11,27 @@ define(
         Brix,
         template
     ) {
-        function Footer () {}
+        function Footer() {}
 
-        _.extend( Footer.prototype, Brix.prototype, {
+        _.extend(Footer.prototype, Brix.prototype, {
             options: {},
             init: function() {},
             render: function() {
-                var html = _.template(template)(this.data)
-                $(this.element).append(html)
+                var that = this
+                $.ajax({
+                    url: 'http://www.taobao.com/go/rgn/mm/footer.php',
+                    dataType: 'jsonp',
+                    jsonp: 'callback',
+                    success: function(resp, status, xhr) {
+                        $(that.element).append(
+                            $('<textarea />').html(resp).val()
+                        )
+                    }
+                })
+
+                // http://www.taobao.com/go/rgn/mm/footer.php?callback=jsonp189
+                // var html = _.template(template)(this.data)
+                // $(this.element).append(html)
             }
         })
 
