4b293dd6fa53e51e20cb61bc69d209cb91ce7f91
幻刺 <huanci.lb@alibaba-inc.com>
2015-11-03 14:23:00 +0800

    save new add of h5 spec

---
+++ h5.spec.components/icons/icons.tpl.js
@@ -0,0 +1,13 @@
+/* global define */
+define(function() {
+    return (function(){/*
+<div class="spec-section mb40 icons">
+    <div class="mb40">
+        <div class="fontsize-20 mb10">尺寸与参考线 <small>Grid and keylines</small></div>
+        <p class="mb20">单个图标基于20x20px区域内绘制，同时保证四周各留2px的边距。反色图标背景区块大小，方形为24px，圆形为28px。图标在页面中使用时需要更具页面场景等比缩放。</p>
+        <img  style="width:100%;" src="https://img.alicdn.com/tps/TB1bNpvKpXXXXa6XXXXXXXXXXXX-1268-728.png"/>
+    </div>
+</div>
+
+    */}).toString().split('\n').slice(1,-1).join('\n') + '\n'
+})
