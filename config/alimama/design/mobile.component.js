e406b19e39ec7efa3e2568edae1ac2faa6f717be
幻刺 <huanci.lb@alibaba-inc.com>
2015-12-09 14:42:03 +0800

    增加移动端左侧列表配置

--- config/alimama/design/mobile.component.js
+++ config/alimama/design/mobile.component.js
@@ -11,12 +11,74 @@ define(function() {
 			role: [],
 			path: 'alimama/design/web/component/index/index',
 			href: '#/design/mobile/component/index'
-		}, {
+		},{
 			name: '按钮',
 			name_en: 'Button',
 			role: [],
 			path: 'alimama/design/mobile/component/button/README.md',
 			href: '#/design/mobile/component/button',
+			prev: 'assets/components/按钮@2x.png'
+		},{
+			name: '导航',
+			name_en: 'navbar',
+			role: [],
+			path: 'alimama/design/mobile/component/navbar/README.md',
+			href: '#/design/mobile/component/navbar'
+		},{
+			name: '标签',
+			name_en: 'tabbar',
+			role: [],
+			path: 'alimama/design/mobile/component/tabbar/README.md',
+			href: '#/design/mobile/component/tabbar',
+			prev: 'assets/components/按钮@2x.png'
+		},{
+			name: '搜索',
+			name_en: 'searchbar',
+			role: [],
+			path: 'alimama/design/mobile/component/searchbar/README.md',
+			href: '#/design/mobile/component/searchbar',
+			prev: 'assets/components/按钮@2x.png'
+		},{
+			name: '表单',
+			name_en: 'form',
+			role: [],
+			path: 'alimama/design/mobile/component/form/README.md',
+			href: '#/design/mobile/component/form',
+			prev: 'assets/components/按钮@2x.png'
+		},{
+			name: '公告',
+			name_en: 'notice',
+			role: [],
+			path: 'alimama/design/mobile/component/notice/README.md',
+			href: '#/design/mobile/component/notice',
+			prev: 'assets/components/按钮@2x.png'
+		},{
+			name: '提示',
+			name_en: 'prompt',
+			role: [],
+			path: 'alimama/design/mobile/component/prompt/README.md',
+			href: '#/design/mobile/component/prompt',
+			prev: 'assets/components/按钮@2x.png'
+		},{
+			name: '列表',
+			name_en: 'tablelist',
+			role: [],
+			path: 'alimama/design/mobile/component/tablelist/README.md',
+			href: '#/design/mobile/component/tablelist',
+			prev: 'assets/components/按钮@2x.png'
+		},{
+			name: '异常页',
+			name_en: 'abnormal',
+			role: [],
+			path: 'alimama/design/mobile/component/abnormal/README.md',
+			href: '#/design/mobile/component/abnormal',
+			prev: 'assets/components/按钮@2x.png'
+		},{
+			name: '分段控件',
+			name_en: 'tabview',
+			role: [],
+			path: 'alimama/design/mobile/component/tabview/README.md',
+			href: '#/design/mobile/component/tabview',
 			prev: 'assets/components/按钮@2x.png'
 		}]
 	}
--- config/alimama/design/mobile.style.js
+++ config/alimama/design/mobile.style.js
@@ -11,6 +11,30 @@ define(function() {
 			role: ['node'],
 			path: 'alimama/design/mobile/style/font/font',
 			href: '#/design/mobile/style/font'
-		}]
+		},{
+            name: '段落',
+			name_en: 'section',
+			role: ['node'],
+			path: 'alimama/design/mobile/style/section/section',
+			href: '#/design/mobile/style/section' 
+        },{
+            name: '图标',
+			name_en: 'icons',
+			role: ['node'],
+			path: 'alimama/design/mobile/style/icons/icons',
+			href: '#/design/mobile/style/icons' 
+        },{
+            name: '度量',
+			name_en: 'metric',
+			role: ['node'],
+			path: 'alimama/design/mobile/style/metric/metric',
+			href: '#/design/mobile/style/metric' 
+        },{
+            name: '结构',
+			name_en: 'struct',
+			role: ['node'],
+			path: 'alimama/design/mobile/style/struct/struct',
+			href: '#/design/mobile/style/struct' 
+        }]
 	}
 })
